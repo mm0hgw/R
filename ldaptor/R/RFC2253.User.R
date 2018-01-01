5a093ed0da518eebb77548ffd6e50f9c10fcf382
Dale <dale@piratepress.org>
2018-01-01 19:34:01 +0000

    buildPackage ldaptor 0.0.0.9001

--- ldaptor/R/RFC2253.User.R
+++ ldaptor/R/RFC2253.User.R
@@ -36,8 +36,7 @@ User.class <- function(x, ...) {
 #'@method User.class default
 User.class.default <- function(x, ...) {
     if (length(x) == 7) 
-        x <- lapply(seq(7), function(i) paste(sep = "", "as.", UserClasses[i])(
-            list(x[[i]])))
+        x <- lapply(seq(7), function(i) paste(sep = "", "as.", UserClasses[i])(list(x[[i]])))
     class(x) <- "User.class"
     print(x)
     if (!valid(x)) 
