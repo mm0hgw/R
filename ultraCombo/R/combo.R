f603405afb21ddae938139a6ed77988eda7e0afd
Dale <dale@piratepress.org>
2018-02-08 21:39:42 +0000

    buildPackage ultraCombo 0.2.1

--- ultraCombo/R/combo.R
+++ ultraCombo/R/combo.R
@@ -25,9 +25,8 @@ ultraCombo <- function(i, n, k) {
             stop("index out of range")
         }
         combnGen(i[x])
-    },
-    sample=function(sampleSize=1000)ultraCombo(i[urandCap(length(i),sampleSize)],n,k)
-    )
+    }, sample = function(sampleSize = 1000) ultraCombo(i[urandCap(length(i), sampleSize)], 
+        n, k))
     class(out) <- "ultraCombo"
     out
 }
