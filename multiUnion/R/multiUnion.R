653d13ac1f085fa22f26bf1a875f0d4042969668
Dale <dale@piratepress.org>
2021-06-21 13:08:13 +0100

    multiUnion 0.0.0.9000 initial commit

--- multiUnion/R/multiUnion.R
+++ multiUnion/R/multiUnion.R
@@ -1,23 +1,23 @@
 #'multiUnion-package
 #'@aliases NULL
-'_PACKAGE'
+"_PACKAGE"
 
 #'multiUnion
 #'@description A union function 
 #' which takes multiple arguments.
 #'@param ... vectors of values
 #'@export
-multiUnion <- function(...){
-	unique(c(...))
+multiUnion <- function(...) {
+    unique(c(...))
 }
 
 #'multiIntersect
 #'@param ... vectors of values
 #'@export
-multiIntersect <- function(...){
-	l1<-list(...)
-	u<-do.call(multiUnion,l1)
-	l2 <- lapply(l1,setdiff,x=u)
-	l3 <- do.call(multiUnion,l2)
-	setdiff(u,l3)
+multiIntersect <- function(...) {
+    l1 <- list(...)
+    u <- do.call(multiUnion, l1)
+    l2 <- lapply(l1, setdiff, x = u)
+    l3 <- do.call(multiUnion, l2)
+    setdiff(u, l3)
 }
