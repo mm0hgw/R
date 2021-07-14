276d53321ef5e7e6b96c251d4d441bf28e6e28b7
Dale <dale@piratepress.org>
2021-07-14 15:40:14 +0100

    Remove get/set chunkSize() functions. Refactor chunk() for sensible operation when len < threads.
    Some docs.

--- getLapply/R/getLapply.R
+++ getLapply/R/getLapply.R
@@ -26,7 +26,7 @@ setLapply <- function(x = lapply) {
 
 #'getSeededLapply
 #'@description
-#'Get a lapply
+#'Get a lapply with an explicity unique PRNG seed.
 #'@export
 getSeededLapply <- function() {
     get("seededLapply", envir = getLapplyEnv)
@@ -34,7 +34,7 @@ getSeededLapply <- function() {
 
 #'setSeededLapply
 #'@description
-#'Set the served lapply.
+#'Set the served lapply with an explicity unique PRNG seed.
 #'@param x a 'function' 
 #'@export
 setSeededLapply <- function(x = lapply) {
@@ -42,37 +42,17 @@ setSeededLapply <- function(x = lapply) {
     assign("seededLapply", x, envir = getLapplyEnv)
 }
 
-#'getChunkSize
-#'@description
-#'Get a chunkSize
-#'@export
-getChunkSize <- function() {
-    get("chunkSize", envir = getLapplyEnv)
-}
-
-#'setChunkSize
-#'@description
-#'Set the served chunkSize
-#'@param x an 'integer' defining a sensible max chunk size.
-#' Default : \code{.Machine$integer.max}
-#'@export
-setChunkSize <- function(x = 2^24) {
-    stopifnot(length(x) == 1)
-    x <- as.integer(x)
-    stopifnot(is.integer(x))
-    stopifnot(!is.na(x))
-    stopifnot(x > 0)
-    assign("chunkSize", x, envir = getLapplyEnv)
-}
-
-
 #'getSensibleThreads
+#'@description
+#'Get the sensible number of threads to plan to run concurrently.
 #'@export
 getSensibleThreads <- function() {
     get("sensibleThreads", envir = getLapplyEnv)
 }
 
 #'setSensibleThreads
+#'@description
+#'Get the sensible number of threads to plan to run concurrently.
 #'@param x a positive 'integer' defining a sensible number of threads to run. 
 #'  Default : \code{max(1,parallel::detectCores()-1)}
 #'@importfrom parallel detectCores
@@ -84,21 +64,26 @@ setSensibleThreads <- function(x = max(1, parallel::detectCores() - 1)) {
     stopifnot(!is.na(x))
     stopifnot(x > 0)
     assign("sensibleThreads", x, envir = getLapplyEnv)
-
 }
 
 #'chunk
 #'@param len the index range to divide
 #'@param threads the number of chunks to create
-#'@return a list of c(start,end) vectors defining chunks
+#'@return a list (even if there's just one) of lists of (start,end) 
+#'arguments useable via do.call(seq,x)
 #'@export
 chunk <- function(len, threads = getSensibleThreads()) {
     stopifnot(length(len) == 1 && len%%1 != 0)
     stopifnot(length(threads) == 1 && threads%%1 != 0)
-    chunkSize <- len%/%threads
 
-    if (chunkSize < 1 || threads < 2) 
+    if (threads < 2) 
         return(list(list(start = 1, end = len)))
+
+    chunkSize <- len%/%threads
+    if (chunkSize < 1) {
+        chunkSize <- 1
+        threads <- len
+    }
 
     seqEnd <- seq(threads) * chunkSize + len - threads * chunkSize
     seqStart <- seqEnd - chunkSize + 1
--- getLapply/R/zzz.R
+++ getLapply/R/zzz.R
@@ -1,4 +1,3 @@
 setSensibleThreads()
-setChunkSize()
 setLapply()
 setSeededLapply()
