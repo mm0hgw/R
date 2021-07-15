d1d2f824216d8162758200a54fc5ead707f1ffec
Dale <dale@piratepress.org>
2021-07-15 14:01:36 +0100

    Typo in sanity checks

--- getLapply/R/chunk.R
+++ getLapply/R/chunk.R
@@ -5,8 +5,8 @@
 #'arguments useable via do.call(seq,x)
 #'@export
 chunk <- function(len, threads = getSensibleThreads()) {
-    stopifnot(length(len) == 1 && len%%1 != 0)
-    stopifnot(length(threads) == 1 && threads%%1 != 0)
+    stopifnot(length(len) == 1 && len%%1 == 0)
+    stopifnot(length(threads) == 1 && threads%%1 == 0)
 
     if (threads < 2) 
         return(list(list(start = 1, end = len)))
@@ -21,5 +21,5 @@ chunk <- function(len, threads = getSensibleThreads()) {
     seqStart <- seqEnd - chunkSize + 1
     seqStart[1] <- 1
 
-    lapply(seq(threads), function(y) list(start = seqStart[y], end = seqEnd[y]))
+    lapply(seq(threads), function(y) list(from = seqStart[y], to = seqEnd[y]))
 }
