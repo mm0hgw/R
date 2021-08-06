15c17c4356e191543eae98c67a9ee7decccb678a
Dale <dale@piratepress.org>
2021-08-06 03:54:56 +0100

    add result labelling

--- ultraCombo/R/dataCombo.R
+++ ultraCombo/R/dataCombo.R
@@ -26,7 +26,22 @@ dataCombo <- function(combo, dataObj) {
         stopifnot(is.function(FUN) || is.primitive(FUN))
         WRAPPEDFUN <- wrappedFunGen(FUN)
         FORKBOMBFUN <- forkBombGen(WRAPPEDFUN, COLLATEFUN)
-        FORKBOMBFUN(combo$i)
+        out <- FORKBOMBFUN(combo$i)
+        if (is.null(dim(out))) {
+            if (length(out) == combo$len) {
+                names(out) <- combo$i
+            }
+        } else {
+            d <- dim(out)
+            if (d[1] == combo$len) {
+                rownames(out) <- combo$i
+            } else {
+                if (d[2] == combo$len) {
+                  colnames(out) <- combo$i
+                }
+            }
+        }
+        out
     }
     out$dataObj <- dataObj
     class(out) <- c("dataCombo", class(out))
