48b170f96bf731824de6024b130a7c427f26ff46
Dale <dale@piratepress.org>
2022-12-08 03:01:19 +0000

    refactor chunk

--- getLapply/R/chunk.R
+++ getLapply/R/chunk.R
@@ -1,21 +1,29 @@
 #'chunk
-#'@param len the index range to divide
+#'Trying to set both threads and chunkSize will return an error. 
+#'If you know both, you don't need this.
+#'@param len the length if the index range to chunk
+#'@param chunkSize the size of chunks to create
 #'@param threads the number of chunks to create
-#'@return a list (even if there's just one) of lists of (start,end) 
+#'@return a list (even if there's just one) of lists of (from,to)  
 #'arguments useable via do.call(seq,x)
 #'@export
-chunk <- function(len, threads = getSensibleThreads()) {
+chunk <- function(len, chunkSize = NULL, threads = NULL) {
     stopifnot(length(len) == 1 && len%%1 == 0)
-    stopifnot(length(threads) == 1 && threads%%1 == 0)
-
-    if (threads < 2)
-        return(list(list(start = 1, end = len)))
-
-    chunkSize <- len%/%threads
-    if (chunkSize < 1) {
-        chunkSize <- 1
-        threads <- len
+    if (!is.null(chunkSize)) {
+        stopifnot(is.null(threads))
+        stopifnot(chunkSize%%1 == 0 && chunkSize > 0)
+        threads <- len%/%chunkSize
+    } else {
+        if (!is.null(threads)) {
+            stopifnot(threads%%1 == 0 && threads > 0)
+        } else {
+            threads <- getSensibleThreads()
+        }
+        chunkSize <- len%/%threads
     }
+
+    if (len < chunkSize)
+        return(list(list(from = 1, to = len)))
 
     seqEnd <- seq(threads) * chunkSize + len - threads * chunkSize
     seqStart <- seqEnd - chunkSize + 1
